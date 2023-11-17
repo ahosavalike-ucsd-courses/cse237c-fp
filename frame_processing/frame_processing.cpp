c33fa235a6ab440731258fb68ec1031b6b917fa9
Adyanth H <33192449+adyanth@users.noreply.github.com>
2023-11-17 13:40:51 -0800

    Remove FIFO depth

--- frame_processing/frame_processing.cpp
+++ frame_processing/frame_processing.cpp
@@ -2,7 +2,7 @@
 #include "gfx_engine.h"
 
 void frame_processing(hls::stream<pixel> &output, hls::stream<pixel> &input) {
-#pragma HLS INTERFACE axis port=output,input depth=1024
+#pragma HLS INTERFACE axis port=output,input
 #pragma HLS PIPELINE II=1
 	static GfxEngine g;
 	g
