42f3d49b3452e2a02d2dcf36cb16a97372e75213
Adyanth H <33192449+adyanth@users.noreply.github.com>
2023-11-17 19:26:06 -0800

    add valid to tb

--- frame_processing/frame_processing.h
+++ frame_processing/frame_processing.h
@@ -1,3 +1,3 @@
 #include "consts_types.h"
 
-void frame_processing(hls::stream<pixel> &output, hls::stream<pixel> &input);
+void frame_processing(hls::stream<pixel> &output, hls::stream<pixel> &input, ap_uint<1> &valid);
