74439c4449357235e9b4975ba60fc589b9396f94
Adyanth H <33192449+adyanth@users.noreply.github.com>
2023-12-04 21:32:23 -0800

    Tetris good

--- frame_processing/tetris.h
+++ frame_processing/tetris.h
@@ -2,6 +2,8 @@
 #define __TETRIS__
 #include "gfx_engine.h"
 
+#define RANDOM_BIT 6
+
 typedef struct TetrisBlock {
     ap_uint<4> x, y;
     ap_uint<2> direction;  // 0 up, 1 right, 2 down, 3 left
@@ -11,8 +13,8 @@ typedef struct TetrisBlock {
 typedef struct TetrisGame {
     GfxEngine g;
     ap_uint<11> score;
-    ap_uint<8> random_reg;
-    ap_uint<3> random_reg_index;
+    ap_uint<1 << RANDOM_BIT> random_reg;
+    ap_uint<RANDOM_BIT> random_reg_index;
     ap_uint<3> world[256];  // 16 * 16
     ap_uint<5> filled_counts[16];
     TetrisBlock active;
@@ -25,7 +27,7 @@ typedef struct TetrisGame {
     void draw_world(hls::stream<streaming_data> &input, hls::stream<streaming_data> &output);
     void get_subsquare(ap_uint<4> &x, ap_uint<4> &y, ap_uint<2> direction, ap_uint<2> id);
     void rotate(ap_uint<2> turn);
-    void move(ap_uint<2> direction);
+    void move(ap_int<2> direction);
     void run(hls::stream<pixel> &input, hls::stream<pixel> &output, ap_uint<2> move);
 } TetrisGame;
 
